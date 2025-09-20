eda64e21560d6f0c03b8b81b35e11cadc3b2248b
jiangwei199805058112-debug <jiangwei199805058112@gmail.com>
2025-09-20 22:35:04 +0900

    chore: add Kotlin DSL Gradle configs and necessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,22 @@
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.ksp) apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
+import org.gradle.api.tasks.Delete
+
+val kotlinVersion: String by extra("1.9.10")
+val composeVersion: String by extra("1.5.0")
+val material3Version: String by extra("1.1.0")
+val lifecycleVersion: String by extra("2.6.1")
+val media3Version: String by extra("1.2.3")
+val exoplayerVersion: String by extra("2.18.7")
+val coilVersion: String by extra("2.4.0")
+val jcifsVersion: String by extra("1.3.19")
+
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
